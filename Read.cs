66608c64fc13574dbb66e901d8c4c5e5b0e217a3
microestc <zengling@bjsasc.com>
2020-10-27 20:32:40 +0800

    ssd

--- Read.cs
+++ Read.cs
@@ -3,5 +3,13 @@ namespace First
     public class Read
     {
         public string Id {get;set;}
+
+        private int myVar;
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        
     }
 }
