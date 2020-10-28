cc9dd18ed1e895e3826ab552cefb7ece7dd08185
microestc <zengling@bjsasc.com>
2020-10-28 15:38:13 +0800

    update

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -6,8 +6,9 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
+            Assert.IsFalse(false, "1 should not be prime");
         }
     }
 }
